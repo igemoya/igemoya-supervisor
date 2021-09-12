916adec366f53b2c2faff1a5c304105b3ea6c43d
HyeokjinKang <coupy@croissant.tech>
2021-09-12 14:23:52 +0900

    호스트를 이렇게 하면 될까~

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 export default defineConfig({
   plugins: [svelte()],
   server: {
-    host: "0.0.0.0",
     port: process.env.port || 80,
   },
 });
