e2e713aae32599adf6c9ae175fecc3d7c0464f9d
HyeokjinKang <coupy@croissant.tech>
2021-09-13 21:56:30 +0900

    로그인 성공시 리다이렉트

--- src/routes/oauth/Index.svelte
+++ src/routes/oauth/Index.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
   import "axios";
-import axios from "axios";
+  import axios from "axios";
+  import { navigate } from "svelte-routing";
+
   const urlParams = new URLSearchParams(window.location.search);
   const code = urlParams.get("code");
   axios.get(`https://igemoya-backend.herokuapp.com/oauth?code=${code}`)
   .then((res) => {
-    console.log(res);
+    if(res.status == 200) {
+      localStorage.me = {
+        "jwt": res.data.jwt,
+        "me": res.data.me
+      };
+      navigate('/supervisor', { replace: true });
+    } else {
+      alert("Error occured.");
+    }
   })
   .catch((error) => {
     console.error(error);
